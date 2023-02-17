89d590920ff59014a46587119e0787bc7fca5d5f
maxilar20 <mael.dorne@outlook.com>
2023-02-17 11:18:28 +0000

    Ball ll dribbling added

--- controllers/SimController/Objects/Field.py
+++ controllers/SimController/Objects/Field.py
@@ -2,6 +2,9 @@ class Field:
     def __init__(self, boundaries):
         self.boundaries = boundaries
 
+    def getCenterPosition(self, field="field"):
+        return (self.boundaries[field][0] + self.boundaries[field][1]) / 2
+
     def isInside(self, pos, field="field"):
         boundary = self.boundaries[field]
         return (
--- controllers/SimController/coach.py
+++ controllers/SimController/coach.py
@@ -13,37 +13,59 @@ class Coach:
         self.ball = ball
 
     def act(self, GUI):
+
+        goal_name = "goal_blue" if self.own_players[0].team == "red" else "goal_red"
+        ball_mov_vector = self.ballPlan(GUI, goal_name)
+        dribbling_pos = ball_mov_vector.normalize() * -0.3
+        self.show(GUI, self.ball, ball_mov_vector)
+        self.show(GUI, self.ball, dribbling_pos, color=[255, 0, 0])
+
         for player in self.own_players:
             if player.player_position == "attacker_right":
-                avoid_vector = self.avoid(player, self.all_players)
-                pursue_vector = self.pursue(player, self.ball.getPosition())
-                mov_vector = avoid_vector + pursue_vector
-                look_vector = mov_vector
+
+                goal_pos = dribbling_pos + self.ball.getPosition()
+
+                if (goal_pos - player.getPosition()).magnitude() > 0.15:
+                    avoid_vector = self.avoid(player, self.all_players, 1)
+                    pursue_vector = self.pursue(player, goal_pos)
+                    mov_vector = avoid_vector + pursue_vector
+                    look_vector = mov_vector
+                else:
+                    pursue_vector = self.pursue(
+                        player, ball_mov_vector + self.ball.getPosition()
+                    )
+                    mov_vector = pursue_vector
+                    look_vector = self.pursue(player, self.ball.getPosition())
             else:
-                avoid_vector = self.avoid(player, self.all_players)
+                avoid_vector = self.avoid(player, self.all_players, 1)
                 mov_vector = avoid_vector
                 look_vector = self.pursue(player, self.ball.getPosition())
 
-            self.show(GUI, player, mov_vector)
-
-            # Send
             mov_vector = mov_vector.clamp_magnitude(1)
             mov_vector_rotated = self.transformToPlayer(player, mov_vector)
 
             look_vector_rotated = self.transformToPlayer(player, look_vector)
+
             rot = np.interp(look_vector_rotated.as_polar()[1], [-180, 180], [2, -2])
             rot = max(min(rot, 1), -1)
 
+            self.show(GUI, player, mov_vector)
             player.act(mov_vector_rotated, rot)
 
-    def avoid(self, own, others):
+    def ballPlan(self, GUI, field):
+        avoid_vector = self.avoid(self.ball, self.enemy_players, 1.5)
+        pursue_vector = self.pursue(self.ball, self.field.getCenterPosition(field))
+        mov_vector = avoid_vector + pursue_vector
+        return mov_vector
+
+    def avoid(self, own, others, dist):
         avoid_vector = math.Vector2(0.001)
 
         for other in others:
             if own.name != other.name:
                 dif_vector = own.getPosition() - other.getPosition()
-                dif_vector = dif_vector.clamp_magnitude(1)
-                dif_vector.scale_to_length(1 - dif_vector.magnitude())
+                dif_vector = dif_vector.clamp_magnitude(dist)
+                dif_vector.scale_to_length(dist - dif_vector.magnitude())
                 avoid_vector += dif_vector
         return avoid_vector
 
@@ -56,10 +78,10 @@ class Coach:
         angle = mt.radians(vector.as_polar()[1]) - player.getOrientation()
         return vector.magnitude() * math.Vector2(mt.cos(angle), mt.sin(angle))
 
-    def show(self, GUI, player, goal):
+    def show(self, GUI, player, goal, color=[0, 255, 0]):
         pygame.draw.line(
             GUI.screen,
-            (0, 255, 0),
+            color,
             GUI.mapToGUI(player.getPosition()),
             GUI.mapToGUI(player.getPosition() + goal),
             1,
